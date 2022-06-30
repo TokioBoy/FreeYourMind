7705f0d1da5284a7a096658faf8ceab7ef675e8b
Vlad <70889664+TokioBoy@users.noreply.github.com>
2022-06-30 12:59:26 +0200

    upd

--- hero.js
+++ hero.js
@@ -3,13 +3,27 @@
     const hero = Snap('#hero');
 
 
-    const circle = hero.circle('50%' , '50%' , 50);
-    // Создание круга по параметрам. 1 и 2 - отступы. 3 - размер 
-    circle.attr({
-        fill: 'red'
-        // fill - заливает цветом
-    });
+   
 
-    circle.animate();
+
+    function drawCircle(color) {
+
+        const circle = hero.circle('50%' , '50%' , 50);
+        // Создание круга по параметрам. 1 и 2 - отступы. 3 - размер 
+        circle.attr({
+            fill: color
+            // fill - заливает цветом
+        });
+    
+        circle.animate({ r: 200}, 6000);
+        // 1 - до какого радиуса , 2 - секунды анимации
+
+
+
+    }
+
+    drawCircle('green');
+
+
 
 })();
